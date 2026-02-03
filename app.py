89b644651ba502d3a09373197fb8ef9c676680f0
nivi12363 <snivedha@tisb.ac.in>
2026-02-03 22:09:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import streamlit.components.v1 as components
 import requests
 
 st.set_page_config(page_title="EcoLens", page_icon="🌱", layout="wide")
